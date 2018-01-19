5227fa426580217848efa5d3e8f5615d4b75df9d
gregnau <gregnau@live.nl>
2018-01-19 12:34:15 +0100

    Initial commit

---
+++ app/src/main/java/tk/bennekom58/kontrol/MainActivity.java
@@ -0,0 +1,62 @@
+package tk.bennekom58.kontrol;
+
+import android.app.Activity;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private SwipeRefreshLayout mySwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Connect to the SwipeRefreshLayout
+        mySwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // Connect the WebView element
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        final Activity activity = this;
+
+        // Enable Javascript in WebView
+        // WebSettings webSettings = mWebView.getSettings();
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        mWebView.loadUrl("https://kontrol.bennekom58.tk/");
+
+        // Create onRefreshListener for pulldown gesture
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        mWebView.reload();
+                    }
+                }
+        );
+
+        // Stop refresh if page loaded
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView web, String url) {
+                if (mySwipeRefreshLayout.isRefreshing()) {
+                    mySwipeRefreshLayout.setRefreshing(false);
+                }
+            }
+        });
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
